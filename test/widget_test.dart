676dfa79a21140a46fb27fb04f4539485a86eb1d
Marco Papula <ga84div@mytum.de>
2020-07-01 00:21:33 +0200

    remove comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:bloc_test/bloc_test.dart';
 import 'package:bloc_test_demo/bloc/my_bloc_bloc.dart';
 import 'package:bloc_test_demo/concrete_value_object.dart';
